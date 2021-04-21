f1bc947aa5ce4f7a51acbc48703f2acf7cb72049
Alex Cook <apcook7104@gmail.com>
2021-04-20 22:15:12 -0700

    Add function doc string

--- array/array.go
+++ array/array.go
@@ -1,5 +1,8 @@
 package array
 
+// SlicesHaveCommonItem determines whether the two input slices
+// have at least one common element.
+// It has linear time complexity O(n) and linear space complexity O(n).
 func SlicesHaveCommonItem(s1, s2 []int) bool {
 	s1Values := make(map[int]bool)
 

ce203380979009a302f965721a765e007ab23055
Alex Cook <apcook7104@gmail.com>
2021-04-21 18:05:57 -0700

    Add array operation tests

--- array/array_test.go
+++ array/array_test.go
@@ -1,6 +1,9 @@
 package array
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSlicesHaveCommonItem(t *testing.T) {
 	tests := []struct {
@@ -16,6 +19,50 @@ func TestSlicesHaveCommonItem(t *testing.T) {
 
 	for _, test := range tests {
 		if got := SlicesHaveCommonItem(test.s1, test.s2); got != test.want {
+			t.Fatalf("want %v, got %v", test.want, got)
+		}
+	}
+}
+
+func TestBasicArrayOperations(t *testing.T) {
+	BasicArrayOperations()
+}
+
+func TestInsert(t *testing.T) {
+	tests := []struct {
+		data  []rune
+		index int
+		value rune
+		want  []rune
+	}{
+		{[]rune{'a', 'b', 'c'}, 0, 'z', []rune{'z', 'a', 'b', 'c'}},
+		{[]rune{'a', 'b', 'c'}, 1, 'z', []rune{'a', 'z', 'b', 'c'}},
+		{[]rune{'a', 'b', 'c'}, 2, 'z', []rune{'a', 'b', 'z', 'c'}},
+		{[]rune{'a', 'b', 'c'}, 3, 'z', []rune{'a', 'b', 'c', 'z'}},
+		{[]rune{}, 5, 'z', []rune{'z'}},
+		{nil, 5, 'z', []rune{'z'}},
+	}
+
+	for _, test := range tests {
+		if got := Insert(test.data, test.index, test.value); !reflect.DeepEqual(got, test.want) {
+			t.Fatalf("want %v, got %v", test.want, got)
+		}
+	}
+}
+
+func TestDelete(t *testing.T) {
+	tests := []struct {
+		data  []rune
+		index int
+		want  []rune
+	}{
+		{[]rune{'a', 'b', 'c'}, 0, []rune{'b', 'c'}},
+		{[]rune{'a', 'b', 'c'}, 1, []rune{'a', 'c'}},
+		{[]rune{'a', 'b', 'c'}, 2, []rune{'a', 'b'}},
+	}
+
+	for _, test := range tests {
+		if got := Delete(test.data, test.index); !reflect.DeepEqual(test.want, got) {
 			t.Fatalf("want %v, got %v", test.want, got)
 		}
 	}

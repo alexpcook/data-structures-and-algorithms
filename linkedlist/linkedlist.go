0397a36af0b95579c38d60860c36f3908c60793d
Alex Cook <apcook7104@gmail.com>
2021-04-23 10:29:23 -0700

    Add linked list operation complexity

---
+++ linkedlist/linkedlist.go
@@ -0,0 +1,8 @@
+package linkedlist
+
+// A linked list contains a set of items, each with a pointer to the next item.
+// - Prepend O(1)
+// - Append O(1)
+// - Lookup O(n)
+// - Insert O(n)
+// - Delete O(n)

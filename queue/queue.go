4cf6894b162f9cc6deebbe4323f0be073fd8ffd5
Alex Cook <apcook7104@gmail.com>
2021-04-24 16:53:19 -0700

    Add queues theory

---
+++ queue/queue.go
@@ -0,0 +1,7 @@
+package queue
+
+// Queues operate on a first-in, first-out (FIFO) paradigm
+// - Lookup O(n) (not a typical operation)
+// - Enqueue O(1) (similar to push, adds an entry to the queue)
+// - Dequeue O(1) (similar to pop, removes the entry that was added first to the queue)
+// - Peek O(1) (view the first item to come out)

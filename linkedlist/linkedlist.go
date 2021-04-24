27549f489f4a4db1c58eb6771291769a38f3e55b
Alex Cook <apcook7104@gmail.com>
2021-04-23 17:27:55 -0700

    Add doubly linked list theory

--- linkedlist/linkedlist.go
+++ linkedlist/linkedlist.go
@@ -1,6 +1,15 @@
 package linkedlist
 
-// A linked list contains a set of items, each with a pointer to the next item.
+// A singly linked list contains a set of items, each with a pointer to the next item.
+// - Prepend O(1)
+// - Append O(1)
+// - Lookup O(n)
+// - Insert O(n)
+// - Delete O(n)
+
+// A doubly linked list contains a set of items, each with a pointer to the next item
+// and a pointer to the previous item. This can make lookup, insert, and delete slightly
+// more efficient (O(n/2) instead of O(n)) if we know in which half of the list the value resides.
 // - Prepend O(1)
 // - Append O(1)
 // - Lookup O(n)

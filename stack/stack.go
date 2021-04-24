8d6831453eb464827ca6f2fb9118c92305d8b780
Alex Cook <apcook7104@gmail.com>
2021-04-24 16:52:39 -0700

    Add stacks theory

---
+++ stack/stack.go
@@ -0,0 +1,7 @@
+package stack
+
+// Stacks operate on a last-in, first-out (LIFO) paradigm
+// - Lookup O(n) (not a typical operation)
+// - Pop O(1) (remove top item)
+// - Push O(1) (add item to top)
+// - Peek O(1) (view the topmost item in the stack)

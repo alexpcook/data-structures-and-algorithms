3c64d32818ee862436d3c7b56ec3677c4baadb3a
Alex Cook <apcook7104@gmail.com>
2021-04-24 23:02:59 -0700

    Enhance queue as stack soln based on examples

--- queue/custom_queue_as_stack.go
+++ queue/custom_queue_as_stack.go
@@ -8,40 +8,41 @@ import (
 
 // QueueAsStack implements a queue as a stack.
 type QueueAsStack struct {
-	data   stack.Stack1
-	length int
+	input  stack.Stack2
+	output stack.Stack2
 }
 
 // String implements the fmt.Stringer interface on *QueueAsStack.
 func (q *QueueAsStack) String() string {
-	return q.data.String()
+	q.Peek()
+	return q.output.String()
 }
 
 // Peek previews the next entry in the queue without dequeueing it.
-// It has time complexity O(1).
+// It has time complexity O(1) if the output stack already has data, else O(n) to transfer input to output.
 func (q *QueueAsStack) Peek() (string, error) {
-	return q.data.Peek()
+	if q.output.Length() == 0 {
+		for next, err := q.input.Pop(); !errors.Is(err, stack.StackIsEmptyError{}); next, err = q.input.Pop() {
+			q.output.Push(next)
+		}
+	}
+	return q.output.Peek()
 }
 
 // Enqueue adds an entry to the queue.
-// It has time complexity O(n) because it has to reverse the order of the underlying stack to preserve
-// the FIFO behavior of a queue.
+// It has time complexity O(1).
 func (q *QueueAsStack) Enqueue(entry string) {
-	for next, err := q.data.Pop(); !errors.Is(err, stack.StackIsEmptyError{}); next, err = q.data.Pop() {
-		defer func(nextEntry string) {
-			q.data.Push(nextEntry)
-		}(next)
-	}
-	q.data.Push(entry)
-	q.length++
+	q.input.Push(entry)
 }
 
 // Dequeue removes an entry from the queue. It returns a non-nil error if the queue is empty.
-// It has time complexity O(1).
+// It has time complexity O(1) if the output stack already has data, else O(n) to transfer input to output.
 func (q *QueueAsStack) Dequeue() (string, error) {
-	val, err := q.data.Pop()
-	if err == nil {
-		q.length--
-	}
-	return val, err
+	q.Peek()
+	return q.output.Pop()
+}
+
+// Length retrieves the number of entries in the queue.
+func (q *QueueAsStack) Length() int {
+	return q.input.Length() + q.output.Length()
 }

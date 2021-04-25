fda738d4bbe7a3582a7a545dc700363d6e5bef89
Alex Cook <apcook7104@gmail.com>
2021-04-25 11:22:02 -0700

    Add binary search tree theory

--- tree/binary_tree.go
+++ tree/binary_tree.go
@@ -7,3 +7,8 @@ package tree
 //
 // Number of nodes = 2^h - 1
 // Number of traversals to find a given node = logN
+//
+// Rules of binary search trees
+// - Any node can have up to two children
+// - The left child node is less than the current node
+// - The right child node is greater than the current node

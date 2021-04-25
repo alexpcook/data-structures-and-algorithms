745fe17eb5df1437d6893fbe6159a00b528c772e
Alex Cook <apcook7104@gmail.com>
2021-04-25 11:46:32 -0700

    Add bst skeleton and constructor

--- tree/binary_tree.go
+++ tree/binary_tree.go
@@ -17,3 +17,43 @@ package tree
 // - Any node can have up to two children
 // - The left child node is less than the current node
 // - The right child node is greater than the current node
+
+type node struct {
+	value int
+	left  *node
+	right *node
+}
+
+// BinarySearchTree represents a BST.
+type BinarySearchTree struct {
+	root   *node
+	height int
+}
+
+// NewBinarySearchTree creates a BST with the given root value and returns a pointer.
+func NewBinarySearchTree(rootValue int) *BinarySearchTree {
+	return &BinarySearchTree{
+		&node{rootValue, nil, nil},
+		1,
+	}
+}
+
+// Insert adds the given value to the BST.
+// It has time complexity O(log(n)) in the average case and O(n) in the worst case.
+func (bst *BinarySearchTree) Insert(value int) {
+
+}
+
+// Lookup determines whether the given value is in the BST.
+// It returns a non-nil error if the value is not present.
+// It has time complexity O(log(n)) in the average case and O(n) in the worst case.
+func (bst *BinarySearchTree) Lookup(value int) error {
+	return nil
+}
+
+// Delete removes the given value from the BST.
+// It returns a non-nil error if the value is not present.
+// It has time complexity O(log(n)) in the average case and O(n) in the worst case.
+func (bst *BinarySearchTree) Delete(value int) error {
+	return nil
+}
--- tree/binary_tree_test.go
+++ tree/binary_tree_test.go
@@ -1,1 +1,21 @@
 package tree
+
+import "testing"
+
+func TestNewBinarySearchTree(t *testing.T) {
+	root := 7
+	bst := NewBinarySearchTree(root)
+
+	if bst.height != 1 {
+		t.Fatalf("want bst height == 1, got bst height == %d", bst.height)
+	}
+	if bst.root.value != root {
+		t.Fatalf("want bst root value == %d, got bst root value == %d", root, bst.root.value)
+	}
+	if bst.root.left != nil {
+		t.Fatalf("want bst root left == nil, got bst root left == %v", bst.root.left)
+	}
+	if bst.root.right != nil {
+		t.Fatalf("want bst root right == nil, got bst root right == %v", bst.root.right)
+	}
+}

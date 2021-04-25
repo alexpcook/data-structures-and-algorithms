bb70a55f902c29a19994b92c35daf698efbc923f
Alex Cook <apcook7104@gmail.com>
2021-04-24 18:17:10 -0700

    Add string and push method unit tests

--- stack/custom_stack_test.go
+++ stack/custom_stack_test.go
@@ -1,12 +1,37 @@
 package stack
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
-func TestPeek(t *testing.T) {
+func TestStack1Peek(t *testing.T) {
 }
 
-func TestPush(t *testing.T) {
+func TestStack1Push(t *testing.T) {
+	s := new(Stack1)
+	s.Push("call1")
+	if sDepth := s.length; sDepth != 1 {
+		t.Fatalf("want stack depth 1, got %d", sDepth)
+	}
+	if s.top == nil || s.bottom == nil || s.top != s.bottom {
+		t.Fatalf("want non-nil and equal stack top and bottom, got top=%#v and bottom=%#v", s.top, s.bottom)
+	}
+
+	s.Push("call2")
+	if sDepth := s.length; sDepth != 2 {
+		t.Fatalf("want stack depth 2, got %d", sDepth)
+	}
+	if s.top == s.bottom {
+		t.Fatalf("want different stack top and bottom, got top=%#v and bottom=%#v", s.top, s.bottom)
+	}
 }
 
-func TestPop(t *testing.T) {
+func TestStack1Pop(t *testing.T) {
+}
+
+func TestStack1String(t *testing.T) {
+	s := new(Stack1)
+	s.Push("call1")
+	fmt.Print(s)
 }

452964a0da968ef96099b3df02f1c979d9d1285e
Alex Cook <apcook7104@gmail.com>
2021-04-24 17:05:17 -0700

    Add more stacks/queues theory

--- queue/queue.go
+++ queue/queue.go
@@ -5,3 +5,7 @@ package queue
 // - Enqueue O(1) (similar to push, adds an entry to the queue)
 // - Dequeue O(1) (similar to pop, removes the entry that was added first to the queue)
 // - Peek O(1) (view the first item to come out)
+
+// Queues can be implemented using either arrays or linked lists
+// Linked lists would be best because you can enqueue/dequeue entries to the head/tail
+// Arrays would require shifting entries, which has O(n) time complexity, so they are NOT a good choice
--- stack/stack.go
+++ stack/stack.go
@@ -5,3 +5,7 @@ package stack
 // - Pop O(1) (remove top item)
 // - Push O(1) (add item to top)
 // - Peek O(1) (view the topmost item in the stack)
+
+// Stacks can be implemented using either arrays or linked lists
+// Arrays work well because you can push/pop entries off the end
+// Linked lists also work if you push/pop entries from the front of the linked list

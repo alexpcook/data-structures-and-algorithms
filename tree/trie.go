16129877ff6b3021fd128f5fe8f288e839ae5860
Alex Cook <apcook7104@gmail.com>
2021-04-25 18:50:29 -0700

    Add trie theory

---
+++ tree/trie.go
@@ -0,0 +1,7 @@
+package tree
+
+// Tries allow you to know whether a word or part of a word exists in a body of text
+// Letters are added in order
+// The complexity of searching for a word is O(length of word)
+// They also have a space advantage because any given letter in position n in a word is stored once instead
+// of multiple times

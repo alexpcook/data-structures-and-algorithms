c8363a44e2725d957f53081f2add0d15ebe22db1
Alex Cook <apcook7104@gmail.com>
2021-04-27 09:44:07 -0700

    Add bubble sort doc string

--- algorithms/sort.go
+++ algorithms/sort.go
@@ -1,5 +1,8 @@
 package algorithms
 
+// BubbleSort sorts the data array in increasing order of integers.
+// It has best case time complexity O(n) and average/worst case time complexity O(n^2).
+// It has space complexity O(1).
 func BubbleSort(data []int) {
 	for i := len(data) - 1; i > 0; i-- {
 		for j := 0; j < i; j++ {

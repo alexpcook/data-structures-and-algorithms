e1b52922d40d0aa26c2afe9cd4452fd7d160bca4
Alex Cook <apcook7104@gmail.com>
2021-04-27 09:46:08 -0700

    Add function to retrieve sort tests

--- algorithms/sort_test.go
+++ algorithms/sort_test.go
@@ -5,17 +5,22 @@ import (
 	"testing"
 )
 
-func TestBubbleSort(t *testing.T) {
-	tests := []struct {
-		input, want []int
-	}{
+type sortTest struct {
+	input, want []int
+}
+
+func sortTests() []sortTest {
+	return []sortTest{
 		{[]int{}, []int{}},
 		{nil, nil},
 		{[]int{1, 4, 2, 3}, []int{1, 2, 3, 4}},
 		{[]int{8, 2, 7, 1}, []int{1, 2, 7, 8}},
 		{[]int{3}, []int{3}},
 	}
+}
 
+func TestBubbleSort(t *testing.T) {
+	tests := sortTests()
 	for i, test := range tests {
 		if BubbleSort(test.input); !reflect.DeepEqual(test.want, test.input) {
 			t.Fatalf("test %d: want %v, got %v", i, test.want, test.input)

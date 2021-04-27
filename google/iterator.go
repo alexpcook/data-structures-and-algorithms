fe26231677f1719167ae77eefeccb110474093d9
Alex Cook <apcook7104@gmail.com>
2021-04-26 19:36:33 -0700

    Implement efficient soln with O(1) next/hasnext

--- google/iterator.go
+++ google/iterator.go
@@ -8,27 +8,41 @@ type Iterator interface {
 	Next() int
 }
 
-type node struct {
+type intIteratorNode struct {
 	value int
-	next  *node
+	next  *intIteratorNode
 }
 
 type IntIterator struct {
-	head *node
+	head *intIteratorNode
 }
 
-// O(1)
-func NewIntIterator(data []int) (*IntIterator, error) {
+// O(n), where n is the length of data
+func NewIntIterator(data []int) *IntIterator {
 	if len(data) == 0 {
-		return nil, fmt.Errorf("got no data")
+		return new(IntIterator)
 	}
-	iter := &IntIterator{&node{data[0], nil}}
+
+	iter := &IntIterator{&intIteratorNode{data[0], nil}}
 	curNode := iter.head
+
 	for i := 1; i < len(data); i++ {
-		curNode.next = &node{data[i], nil}
+		curNode.next = &intIteratorNode{data[i], nil}
 		curNode = curNode.next
 	}
-	return iter, nil
+
+	return iter
+}
+
+// O(n), where n is the number of entries in the linked list
+func (itr *IntIterator) String() string {
+	str := "["
+	currentNode := itr.head
+	for currentNode != nil {
+		str += fmt.Sprintf("%d-->", currentNode.value)
+		currentNode = currentNode.next
+	}
+	return str + "]"
 }
 
 // O(1)
@@ -45,39 +59,75 @@ func (itr *IntIterator) Next() (int, error) {
 	return itr.head.value, nil
 }
 
+type intInterleaverEntry struct {
+	iterator IntIterator
+	next     *intInterleaverEntry
+}
+
 type IntInterleaver struct {
-	data  []IntIterator
-	index int
+	first, last *intInterleaverEntry
+	length      int
+}
+
+// O(n), where n is the number of iterators passed to the constructor
+func NewIntInterleaver(iterators ...IntIterator) *IntInterleaver {
+	interleaver := new(IntInterleaver)
+
+	for _, iter := range iterators {
+		if iter.HasNext() {
+			entry := &intInterleaverEntry{iter, nil}
+			if interleaver.last != nil {
+				interleaver.last.next = entry
+			}
+			interleaver.last = entry
+			if interleaver.length == 0 {
+				interleaver.first = interleaver.last
+			}
+			interleaver.length++
+		}
+	}
+
+	return interleaver
+}
+
+// O(n), where n is the number of iterators in the queue
+// func (itl *IntInterleaver) String() string {
+// 	str := ""
+// 	currentEntry := itl.first
+// 	for currentEntry != nil {
+// 		str += fmt.Sprint(currentEntry.iterator)
+// 		currentEntry = currentEntry.next
+// 	}
+// 	return str
+// }
+
+// O(1)
+func (itl *IntInterleaver) HasNext() bool {
+	return itl.length > 0
 }
 
 // O(1)
-func NewIntInterleaver(iterators ...IntIterator) (*IntInterleaver, error) {
-	if len(iterators) == 0 {
-		return nil, fmt.Errorf("got not iterators")
-	}
-	return &IntInterleaver{iterators, 0}, nil
-}
-
-// O(itl.data)
-func (itl *IntInterleaver) HasNext() bool {
-	for i, j := itl.index, 0; j < len(itl.data); i, j = (i+1)%len(itl.data), j+1 {
-		if itl.data[i].HasNext() {
-			return true
-		}
-	}
-	return false
-}
-
-// O(itl.data)
 func (itl *IntInterleaver) Next() (int, error) {
-	for j := 0; j < len(itl.data); itl.index, j = (itl.index+1)%len(itl.data), j+1 {
-		if itl.data[itl.index].HasNext() {
-			defer func() {
-				itl.index++
-				itl.index %= len(itl.data)
-			}()
-			return itl.data[itl.index].Next()
-		}
+	if itl.length == 0 {
+		return 0, fmt.Errorf("empty interleaver")
 	}
-	return 0, nil
+
+	entry := itl.first
+	itl.first = itl.first.next
+	itl.length--
+
+	defer func() {
+		if entry.iterator.HasNext() {
+			if itl.last != nil {
+				itl.last.next = entry
+			}
+			itl.last = entry
+			if itl.length == 0 {
+				itl.first = itl.last
+			}
+			itl.length++
+		}
+	}()
+
+	return entry.iterator.Next()
 }
--- google/iterator_test.go
+++ google/iterator_test.go
@@ -1,49 +1,91 @@
 package google
 
 import (
+	"fmt"
 	"testing"
 )
 
 func TestIntIterator(t *testing.T) {
 	data := []int{}
-	_, err := NewIntIterator(data)
+	itr := NewIntIterator(data)
+	if itr.HasNext() {
+		t.Fatal("want empty iterator")
+	}
+	_, err := itr.Next()
 	if err == nil {
 		t.Fatal("want error, got nil")
 	}
 
-	data = []int{1}
-	itr, err := NewIntIterator(data)
-	if err != nil {
-		t.Fatal(err)
-	}
-	for i := 0; itr.HasNext(); i++ {
-		got, err := itr.Next()
-		if err != nil {
-			t.Fatal(err)
-		} else if got != data[i] {
-			t.Fatalf("want %d, got %d", data[i], got)
+	tests := [][]int{{1}, {8, 3, 4}}
+	for _, test := range tests {
+		itr = NewIntIterator(test)
+		fmt.Println(itr)
+		for i := 0; itr.HasNext(); i++ {
+			got, err := itr.Next()
+			if err != nil {
+				t.Fatal(err)
+			} else if got != test[i] {
+				t.Fatalf("want %d, got %d", test[i], got)
+			}
+		}
+		_, err = itr.Next()
+		if err == nil {
+			t.Fatal("want error, got nil")
 		}
 	}
-	_, err = itr.Next()
-	if err == nil {
+}
+
+func TestIntInterleaver(t *testing.T) {
+	interleaver := NewIntInterleaver()
+	if interleaver.HasNext() {
+		t.Fatal("want empty interleaver")
+	}
+
+	if _, err := interleaver.Next(); err == nil {
 		t.Fatal("want error, got nil")
 	}
 
-	data = []int{8, 3, 4}
-	itr, err = NewIntIterator(data)
-	if err != nil {
-		t.Fatal(err)
-	}
-	for i := 0; itr.HasNext(); i++ {
-		got, err := itr.Next()
-		if err != nil {
+	emptyIter := NewIntIterator([]int{1})
+	for emptyIter.HasNext() {
+		if _, err := emptyIter.Next(); err != nil {
 			t.Fatal(err)
-		} else if got != data[i] {
-			t.Fatalf("want %d, got %d", data[i], got)
 		}
 	}
-	_, err = itr.Next()
-	if err == nil {
+
+	interleaver = NewIntInterleaver(*emptyIter)
+	if interleaver.HasNext() {
+		t.Fatal("want empty interleaver")
+	}
+	if _, err := interleaver.Next(); err == nil {
+		t.Fatal("want error, got nil")
+	}
+
+	slices := [][]int{
+		{1, 2, 3},
+		{4, 5},
+		{6, 7, 8, 9},
+	}
+	iters := make([]IntIterator, 3)
+	for i := 0; i < len(iters); i++ {
+		iter := NewIntIterator(slices[i])
+		iters[i] = *iter
+	}
+
+	interleaver = NewIntInterleaver(iters...)
+	want := []int{1, 4, 6, 2, 5, 7, 3, 8, 9}
+
+	for i := 0; interleaver.HasNext(); i++ {
+		switch got, err := interleaver.Next(); {
+		case err != nil:
+			t.Fatal(err)
+		case got != want[i]:
+			t.Fatalf("want %d, got %d", want[i], got)
+		default:
+			fmt.Println(got, err, interleaver)
+		}
+	}
+
+	if _, err := interleaver.Next(); err == nil {
 		t.Fatal("want error, got nil")
 	}
 }
